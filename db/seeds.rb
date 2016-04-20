c5ee2dae6722934ba2ef0cc4402d81ac2d361243
vtran14 <vtran14@mail.dupage.edu>
2016-04-20 21:21:57 +0000

    intialize respository

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Quote.create(author:'Arthur C. Clarke',quote:'Any sufficiently advanced technology is indistinguisable from magic.',background:'Arthur C. Clarke')
+Quote.create(author:'Elon Musk',quote:'With artificialintelligence, we are summoning the demon.',background:'Now he makes electric cars.')
+Quote.create(author:'Pablo Picasso',quote:'Computers are useless. They can only give you answers.',background:'Some remarkable cubist paintings he did.')
