314375544ed4af1c514c98964e5780e321d28786
vtran14 <vtran14@mail.dupage.edu>
2016-06-07 23:38:44 +0000

    first commit

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
+
 gem 'rails'
-gem 'sass-rails'
+gem 'bootstrap-sass',       '3.2.0.0'
 gem 'uglifier'
 gem 'coffee-rails'
 gem 'jquery-rails'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,5 +15,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 
 end
